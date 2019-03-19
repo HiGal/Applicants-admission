32c0f034acdccb9487f5c5b8243d1c8586a7af5d
HiGal <f.galeev@innopolis.ru>
2019-03-19 13:22:32 +0300

    Added unit tests

---
+++ tests/test_pack_1.py
@@ -0,0 +1,43 @@
+import unittest
+from faker import Faker
+import app
+import json
+
+class RegistrationTest(unittest.TestCase):
+
+    def setUp(self):
+        app.app.testing = True
+        self.app = app.app.test_client()
+
+    def generate_data(self):
+        faker = Faker()
+        profile = faker.simple_profile()
+        print(profile['name'].split())
+        name, sname = profile['name'].split()[0],profile['name'].split()[1]
+        email = profile['mail']
+        birthdate = profile['birthdate'].strftime("%Y-%m-%d")
+        password = profile['username']
+        data_pack = {
+            'name': name,
+            'sname': sname,
+            'email': email,
+            'bdate': birthdate,
+            'password': password,
+            'cpassword': password
+        }
+        return data_pack
+
+    # Test when password and check password field are the same
+    def test_registration_1(self):
+        data = self.generate_data()
+        rv = self.app.post('/register', data=json.dumps(data), content_type='application/json')
+        assert b'Account successfully created' in rv.data
+
+    def test_registration_2(self):
+        data = self.generate_data()
+        data['cpassword'] = data['cpassword']+'ad'
+        rv = self.app.post('/register', data=json.dumps(data), content_type='application/json')
+        assert b'Password are not the same!' in rv.data
+
+if __name__ == '__main__':
+    unittest.main()
