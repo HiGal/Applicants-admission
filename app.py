61f0979762c3971d7255fd72c1bf2ee5b6230986
Ruslan Shakirov <shakirovrrr@outlook.com>
2019-03-31 00:46:31 +0300

    Passwords are being hashed, but some tests fail

--- app.py
+++ app.py
@@ -1,44 +1,50 @@
-from flask import Flask
-from flask import Flask, redirect, render_template, request, json, Response
+from flask import Flask, redirect, render_template, request, Response
+
 from Models import User
 
 app = Flask(__name__)
 
 
+def hash_password(password: str) -> str:
+	from hashlib import md5
+	return md5(password.encode()).hexdigest()
+
+
 @app.route('/')
 def hello_world():
-    return redirect("/login")
+	return redirect("/login")
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    if request.method == 'POST':
-        data = request.get_json(silent=True)
-        user = User(data['username'], data['password'])
-        if user.verify():
-            return Response("Success!")
-        else:
-            return Response("Username or Password incorrect")
-    return render_template('login.html')
+	if request.method == 'POST':
+		data = request.get_json(silent=True)
+		password = hash_password(data['password'])
+		user = User(data['username'], password)
+		if user.verify():
+			return Response("Success!")
+		else:
+			return Response("Username or Password incorrect")
+	return render_template('login.html')
 
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    if request.method == 'POST':
-        user = User()
-        data = request.get_json(silent=True)
-        name = data['name']
-        sname = data['sname']
-        email = data['email']
-        bdate = data['bdate']
-        if data['password'] == data['cpassword']:
-            password = data['password']
-            user.register(email, password, name, sname, email, bdate)
-            return Response("Account successfully created")
-        else:
-            return Response("Password are not the same!")
-    return render_template('registration.html')
+	if request.method == 'POST':
+		user = User()
+		data = request.get_json(silent=True)
+		name = data['name']
+		sname = data['sname']
+		email = data['email']
+		bdate = data['bdate']
+		if data['password'] == data['cpassword']:
+			password = hash_password(data['password'])
+			user.register(email, password, name, sname, email, bdate)
+			return Response("Account successfully created")
+		else:
+			return Response("Password are not the same!")
+	return render_template('registration.html')
 
 
 if __name__ == '__main__':
-    app.run()
+	app.run()
