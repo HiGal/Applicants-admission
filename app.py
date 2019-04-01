c68b1840e335741f4849e934a090f6fcc62839a8
Egor Klementev <egorklementeff@yandex.ru>
2019-04-01 19:40:48 +0300

    Transition between pages

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, render_template, request, Response
+from flask import Flask, redirect, render_template, request, Response, url_for, jsonify
 
 from Models import User
 
@@ -12,7 +12,7 @@ def hash_password(password: str) -> str:
 
 @app.route('/')
 def hello_world():
-	return redirect("/login")
+	return redirect(url_for('login'))
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -22,7 +22,7 @@ def login():
 		password = hash_password(data['password'])
 		user = User(data['username'], password)
 		if user.verify():
-			return Response("Success!")
+			return jsonify(dict(redirect=url_for("profile", username=data['username'])))
 		else:
 			return Response("Username or Password incorrect")
 	return render_template('login.html')
@@ -40,11 +40,14 @@ def register():
 		if data['password'] == data['cpassword']:
 			password = hash_password(data['password'])
 			user.register(email, password, name, sname, email, bdate)
-			return Response("Account successfully created")
+			return jsonify(dict(redirect=url_for("profile", username=email)))
 		else:
 			return Response("Password are not the same!")
 	return render_template('registration.html')
 
+@app.route('/profile/<username>', methods=['GET', 'POST'])
+def profile(username=None):
+	return render_template('profile.html', username=username)
 
 if __name__ == '__main__':
 	app.run()
