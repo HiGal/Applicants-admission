82ad15d4a17caef68ba361bef08b14e762fd4c6e
Ruslan Shakirov <shakirovrrr@outlook.com>
2019-04-01 20:01:20 +0300

    Secure module

---
+++ Secure.py
@@ -0,0 +1,31 @@
+import base64
+
+from cryptography.fernet import Fernet
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+
+
+def create_key(password: bytes, salt: bytes, length: int = 32) -> bytes:
+	if len(salt) < 16:
+		raise ValueError('Salt should be 16-byte or longer, but it was {} byte.'.format(len(salt)))
+
+	kdf = PBKDF2HMAC(
+		algorithm=hashes.SHA3_256,
+		length=length,
+		salt=salt,
+		iterations=100000,
+		backend=default_backend()
+	)
+
+	return base64.urlsafe_b64encode(kdf.derive(password))
+
+
+def encrypt(data: bytes, key: bytes) -> bytes:
+	fernet = Fernet(key)
+	return fernet.encrypt(data)
+
+
+def decrypt(data: bytes, key: bytes) -> bytes:
+	fernet = Fernet(key)
+	return fernet.decrypt(data)
