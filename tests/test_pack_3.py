bc831a3f6b4df26c03a3b22ee1607f27f94ec5ab
jafar <jafer.ba@Gmail.com>
2019-04-15 20:20:20 +0300

    added tests for uploading attachments
    [BACKEND]

--- tests/test_pack_3.py
+++ tests/test_pack_3.py
@@ -12,13 +12,6 @@ class AddPhoto(unittest.TestCase):
         app.app.testing = True
         self.app = app.app.test_client()
 
-    '''print(photo_binary)
-            encoded = base64.encodebytes(photo_binary)
-            print(encoded)
-            print("\n")
-            zinj = encoded.decode('ascii')
-            # print(zinj)'''
-
     def test_add_test_photo(self):
         try:
             path = os.path.dirname(os.path.realpath(__file__)) + "/test_image.jpg"
@@ -44,6 +37,25 @@ class AddPhoto(unittest.TestCase):
 
     def test_get_profile_picture(self):
         rv = self.app.get('/profile_picture', data=json.dumps([]), content_type='application/json')
+
+        assert b'got the picture' in rv.data
+
+    def test_add_pdf_attachment(self):
+        try:
+            with open("attachment.pdf", 'rb') as f:
+                attachment_binary = f.read()
+            attachment_integer = int.from_bytes(attachment_binary, byteorder='big')
+            data = {
+                'attachment_integer': attachment_integer,
+                'name_of_attachment': "pdf_attachment"
+            }
+            rv = self.app.post('/add_attachment', data=json.dumps(data), content_type='application/json')
+        except IOError:
+            print("File is not there")
+
+    def test_get_pdf_attachment(self):
+
+        rv = self.app.get('/add_attachment', data=json.dumps([]), content_type='application/json')
 
         assert b'got the picture' in rv.data
 
